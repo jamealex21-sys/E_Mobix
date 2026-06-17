714d021710104cd3dcb1280e7891eb9d101e742d
jame <jamealex21@gmail.com>
2026-06-17 20:38:17 +0700

    Remove old project files

--- products/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Product, ProductVariant, CartItem # នាំចូល Model ទាំងអស់ដែលប្អូនមាន
-
-# ចុះឈ្មោះ Model ឱ្យវាបង្ហាញក្នុង Admin Panel
-admin.site.register(Product)
-admin.site.register(ProductVariant)
-admin.site.register(CartItem)
--- products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from products.views import debug_products
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
-    path("debug-products/", debug_products, name='debug_products'),
-]
